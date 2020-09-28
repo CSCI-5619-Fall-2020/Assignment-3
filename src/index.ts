14caf0612db768b7314f87fb557393e5dbb1ef6f
Evan Suma Rosenberg <suma@umn.edu>
2020-09-28 14:45:19 -0500

    initial commit with template code

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+/* CSCI 5619 Assignment 3, Fall 2020
+ * Author: Evan Suma Rosenberg
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { Vector3 } from "@babylonjs/core/Maths/math";
+import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { PointerEventTypes, PointerInfo } from "@babylonjs/core/Events/pointerEvents";
+import { WebXRManagedOutputCanvasOptions } from "@babylonjs/core/XR";
+
+// Side effects
+import "@babylonjs/loaders/glTF/2.0/glTFLoader"
+import "@babylonjs/core/Helpers/sceneHelpers";
+
+// Note: The structure has changed since previous assignments because we need to handle the 
+// async methods used for setting up XR. In particular, "createDefaultXRExperienceAsync" 
+// needs to load models and create various things.  So, the function returns a promise, 
+// which allows you to do other things while it runs.  Because we don't want to continue
+// executing until it finishes, we use "await" to wait for the promise to finish. However,
+// await can only run inside async functions. https://javascript.info/async-await
+class Game 
+{ 
+    private canvas: HTMLCanvasElement;
+    private engine: Engine;
+    private scene: Scene;
+
+    constructor()
+    {
+        // Get the canvas element 
+        this.canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+
+        // Generate the BABYLON 3D engine
+        this.engine = new Engine(this.canvas, true); 
+
+        // Creates a basic Babylon Scene object
+        this.scene = new Scene(this.engine);   
+    }
+
+    start() : void
+    {
+        this.createScene().then(() => {
+            // Register a render loop to repeatedly render the scene
+            this.engine.runRenderLoop(() => { 
+                this.scene.render();
+            });
+
+            // Watch for browser/canvas resize events
+            window.addEventListener("resize", () => { 
+                this.engine.resize();
+            });
+        });
+    }
+
+    private async createScene()
+    {
+        // This creates and positions a first-person camera (non-mesh)
+        var camera = new UniversalCamera("camera1", new Vector3(0, 1.6, 0), this.scene);
+        camera.fov = 90 * Math.PI / 180;
+
+        // This attaches the camera to the canvas
+        camera.attachControl(this.canvas, true);
+
+        // There is a bug in Babylon 4.1 that fails to enable the highlight layer on the Oculus Quest. 
+        // This workaround fixes the problem.
+        var canvasOptions = WebXRManagedOutputCanvasOptions.GetDefaults();
+        canvasOptions.canvasOptions!.stencil = true;
+
+        // Creates the XR experience helper
+        const xrHelper = await this.scene.createDefaultXRExperienceAsync({outputCanvasOptions: canvasOptions});
+
+        // There is a bug in Babylon 4.1 that fails to reenable pointer selection after a teleport
+        // This is a hacky workaround that disables a different unused feature instead
+        xrHelper.teleportation.setSelectionFeature(xrHelper.baseExperience.featuresManager.getEnabledFeature("xr-background-remover"));
+
+        // Register event handler for selection events (pulling the trigger, clicking the mouse button)
+        this.scene.onPointerObservable.add((pointerInfo) => {
+            this.processPointer(pointerInfo);
+        });
+
+        // Add code to create your scene here
+
+    }
+
+    // Event handler for processing pointer selection events
+    private processPointer(pointerInfo: PointerInfo)
+    {
+        switch (pointerInfo.type) {
+            case PointerEventTypes.POINTERDOWN:
+                if (pointerInfo.pickInfo?.hit) {
+
+                    // Add code for selection events here
+
+                }
+                break;
+        }
+    }
+}
+/******* End of the Game class ******/   
+
+// Start the game
+var game = new Game();
+game.start();
